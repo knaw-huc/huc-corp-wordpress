0b428e5f341ef8ffbdfef08dd27d14de6b4066fe
Doppen <basdoppen@gmail.com>
2018-07-05 16:31:03 +0200

    partners

--- src/wp/_inc/_inc-connections.php
+++ src/wp/_inc/_inc-connections.php
@@ -8,11 +8,19 @@ function displayConnection($type) {
     'nopaging' => true,
   ) );
 
+
+
   // Display connected pages
   if ( $connected->have_posts() ) :
 
     while ( $connected->have_posts() ) : $connected->the_post();
-      $output = $output.'<a href="'.get_the_permalink().'">'.get_the_title().'</a>';
+
+
+      if (strpos($type, 'partner') !== false) {
+        $output = $output.'<a href="'.get_post_meta( get_the_ID(), 'wpcf-url', true ).'"><img src="images/'.get_post_meta( get_the_ID(), 'wpcf-image-name', true ).'" /></a>';
+      }else {
+        $output = $output.'<a href="'.get_the_permalink().'">'.get_the_title().'</a>';
+      }
 
     endwhile;
     wp_reset_postdata();
