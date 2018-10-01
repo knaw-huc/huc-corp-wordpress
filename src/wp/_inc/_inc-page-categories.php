b0b1e242418565a736f4309f1ab53cd2b53d35fb
Doppen <basdoppen@gmail.com>
2018-10-01 16:01:15 +0200

    Uncategorized

--- src/wp/_inc/_inc-page-categories.php
+++ src/wp/_inc/_inc-page-categories.php
@@ -5,12 +5,14 @@ $templateTag = 'element-tag.html';
 $tagFormat =  file_get_contents(get_bloginfo('template_directory').'/'.$templateTag);
 
 foreach ($categories as $categorie ) {
-  $tagCode = $tagFormat;
-  $tagCode = str_replace("{{tag-name}}",$categorie->name, $tagCode);
-  $tagCode = str_replace("{{tag-id}}",$categorie->term_id, $tagCode);
-  $tagCode = str_replace("{{tag-count}}",$categorie->category_count, $tagCode);
+  if ($categorie->name != 'Uncategorized') {
+      $tagCode = $tagFormat;
+      $tagCode = str_replace("{{tag-name}}",$categorie->name, $tagCode);
+      $tagCode = str_replace("{{tag-id}}",$categorie->term_id, $tagCode);
+      $tagCode = str_replace("{{tag-count}}",$categorie->category_count, $tagCode);
 
-  $catsString = $catsString.$tagCode.' ';
+      $catsString = $catsString.$tagCode.' ';
+    }
 }
 $postCats = $catsString;
 $catsString ='';
