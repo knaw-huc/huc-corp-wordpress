8da6e279a5c3ace9da4b01eb0add208fe003068c
Doppen <basdoppen@gmail.com>
2018-10-02 12:46:26 +0200

    productname

--- src/wp/_inc/_inc-z-custom-build-custom-fields.php
+++ src/wp/_inc/_inc-z-custom-build-custom-fields.php
@@ -1,6 +1,6 @@
 <?php
   $postCustomMetaKeywords = get_post_meta( get_the_ID(), 'wpcf-extra-keywords', true );
-  $postCustomMetaGithubName = get_post_meta( get_the_ID(), 'wpcf-github-name', true );
+  $postCustomMetaToolName = get_post_meta( get_the_ID(), 'wpcf-tool-name', true );
   $postCustomMetastaffName = get_post_meta( get_the_ID(), 'wpcf-staff-name', true );
   $postCustomMetaFunction = get_post_meta( get_the_ID(), 'wpcf-function', true );
   $postCustomMetaEmail = get_post_meta( get_the_ID(), 'wpcf-email', true );
